c65d333b4e8da7f3dc7d7c84c05929a9371a0bbc
Paul Gallovich <pgallovich@gmail.com>
2026-01-12 11:58:02 -0800

    Remove Kerrie Droban [ZHIVAGO] from featured experts carousel

--- lib/data/experts.ts
+++ lib/data/experts.ts
@@ -2,14 +2,6 @@ import { Expert } from '@/lib/types/expert';
 
 export const FEATURED_EXPERTS: Expert[] = [
   {
-    id: 'kerrie-droban-zhivago',
-    name: 'Kerrie Droban [ZHIVAGO]',
-    title: 'Award-winning True Crime Author/Podcaster, Attorney, and Television Journalist',
-    description:
-      'Kerrie Droban [ZHIVAGO] is an award-winning true crime author/podcaster, attorney, and television journalist.',
-    imageSrc: '/assets/kerrie-droban-zhivago.jpeg',
-  },
-  {
     id: 'aaron-hawkins',
     name: 'Aaron Hawkins',
     title: 'Detective at Portland Oregon Sheriffs Office assigned to CCSO DVERT (Domestic Violence Enhanced Response Team)',
